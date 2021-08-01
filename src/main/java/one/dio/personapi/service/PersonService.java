0bf5cb86d1156e9a00f250f0ffbe15ccfa2b6fde
Lucas <lucasm.miranda@outlook.com>
2021-08-01 18:33:16 -0300

    Projeto DIO

--- src/main/java/one/dio/personapi/service/PersonService.java
+++ src/main/java/one/dio/personapi/service/PersonService.java
@@ -1,24 +1,36 @@
 package one.dio.personapi.service;
 
-import one.dio.personapi.dto.MessageResponseDTO;
+import one.dio.personapi.dto.request.PersonDTO;
+import one.dio.personapi.dto.response.MessageResponseDTO;
 import one.dio.personapi.entity.Person;
+import one.dio.personapi.mapper.PersonMapper;
 import one.dio.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
 
+     private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson( Person person){
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson( PersonDTO personDTO){
+        Person personToSave = personMapper.toModel(personDTO);
+
+//        Person personToSave = Person.builder()
+//                .firstName(personDTO.getFirstName())
+//                .lastName(personDTO.getLastName())
+//                .birthDate(personDTO.getBirthDate())
+//                .phones(personDTO.getPhones())
+//                .build();
+
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Pessoa criada com o ID : " + savedPerson.getId())
